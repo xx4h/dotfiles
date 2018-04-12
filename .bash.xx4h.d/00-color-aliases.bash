53092623f88a96d0c4e7a97c976d1401fd86a84d
Fabian 'xx4h' Melters <xx4h@xx4h.de>
2018-04-12 10:14:30 +0200

    Add escapes to colors to get correct PS1
    columns counting

--- .bash.xx4h.d/00-color-aliases.bash
+++ .bash.xx4h.d/00-color-aliases.bash
@@ -4,16 +4,16 @@
 # always remove all spaces from the above code
 # use xx4hBashGet256Colors to get the color codes for the above example
 
-LIGHT_GRAY="\e[37m"
-LIGHT_PURPLE="\e[34m"
-LIGHT_YELLOW="\e[38;5;190m"
-LIGHT_CYAN="\e[96m"
-YELLOW="\e[38;5;220m"
-WHITE="\e[37m"
-GREEN="\e[32m"
-CYAN="\e[36m"
-RED="\e[31m"
-COLOR_NONE="\e[0m"
-DEFAULT_COLOR="\e[39m"
+LIGHT_GRAY="\[\e[37m\]"
+LIGHT_PURPLE="\[\e[34m\]"
+LIGHT_YELLOW="\[\e[38;5;190m\]"
+LIGHT_CYAN="\[\e[96m\]"
+YELLOW="\[\e[38;5;220m\]"
+WHITE="\[\e[37m\]"
+GREEN="\[\e[32m\]"
+CYAN="\[\e[36m\]"
+RED="\[\e[31m\]"
+COLOR_NONE="\[\e[0m\]"
+DEFAULT_COLOR="\[\e[39m\]"
 
-TEXT_BOLD="\e[1m"
+TEXT_BOLD="\[\e[1m\]"

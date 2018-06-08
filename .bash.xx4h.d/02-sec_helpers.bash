98061e1de2cc2a0f9ddbda2643d891a169267796
Fabian 'xx4h' Melters <xx4h@xx4h.de>
2018-06-08 18:12:33 +0200

    add 02-sec_helpers.bash to xx4h functions
    - sec_check_http_trace_enabled function
    
      a function to check if http method TRACE is enabled.
      it is good practice to disable or at least disallow
      TRACE method

---
+++ .bash.xx4h.d/02-sec_helpers.bash
@@ -0,0 +1,29 @@
+# sec function helpers
+function sec_check_http_trace_enabled() {
+    host="$1"
+    ssl="$2"
+
+    if [ "$host" = "" ]; then
+        echo "Usage: check_http_trace_enabled host:port [ssl]"
+	return
+    fi
+
+    if [ "$ssl" != "" ]; then
+        HTTP="--insecure https"
+	OUTTXT="$host SSL"
+    else
+        HTTP="http"
+	OUTTXT="$host NOSSL"
+    fi
+
+    CURLBASE="curl --connect-timeout 10 -v -X TRACE"
+
+    OUTPUT="$($CURLBASE $HTTP://$host 2>&1 | egrep '^< HTTP/1.1')"
+
+    if [ "$OUTPUT" = "" ]; then
+            OUTRETURN="NORETURN"
+    else
+            OUTRETURN="$(echo $OUTPUT | awk '{print $3}')"
+    fi
+    echo "$OUTTXT $OUTRETURN"
+}

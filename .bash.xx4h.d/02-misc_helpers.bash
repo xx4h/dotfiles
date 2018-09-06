0632eff2e85108f19c5b422025d9923130a71e17
Fabian Melters <fmelters@linux-ag.com>
2018-09-06 15:26:43 +0200

    Move some functions to misc helpers
    - urlencode moved to misc helpers
    - urldecode moved to misc helpers

--- .bash.xx4h.d/02-misc_helpers.bash
+++ .bash.xx4h.d/02-misc_helpers.bash
@@ -11,3 +11,23 @@ function add () {
     # awk '{sum+=$'$COLUMN' ; print $0} END{print "sum=",sum}'
     awk '{sum+=$'$COLUMN'} END{print sum}'
 }
+
+# Return encoded url
+function urlencode() {
+    # urlencode <string>
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf '%%%02X' "'$c"
+        esac
+    done
+}
+
+# Return decoded url
+function urldecode() {
+    : "${*//+/ }"
+    echo -e "${_//%/\\x}"
+}
+

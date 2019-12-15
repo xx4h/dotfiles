d17dc194e5a51ac8069f2679512152bff1b76f98
Fabian 'xx4h' Melters <xx4h@xx4h.de>
2019-12-15 19:39:07 +0100

    Add xx4hBashReload and extend h3lp

--- .bash.xx4h.d/xx4h-functions.bash
+++ .bash.xx4h.d/xx4h-functions.bash
@@ -65,6 +65,10 @@ function xx4hBashTempDisableHomeGitParse() {
     export XX4H_DISABLE_GIT_PARSE_USER_HOME=1
 }
 
+function xx4hBashReload() {
+    source ~/.bashrc
+}
+
 function xtelnet() {
     ( echo "import telnetlib"
       echo "try:"
@@ -101,6 +105,8 @@ function h3lp () {
     echo "  xx4hBashStartProfiling                 | Enable profiling and debug for bash startup (same es xx4hBashStartDebug)"
     echo "  xx4hBashStopDebug                      | Disable profiling and debug for bash startup"
     echo "  xx4hBashStopProfiling                  | Disable profiling and debug for bash startup (same es xx4hBashStopDebug)"
+    echo "  xx4hBashGet256Colors                   | Get all 256 bash colors"
+    echo "  xx4hBashReload                         | Reload ~/.bashrc (re-source)"
     echo ""
     echo "Some functions:"
     echo "  xx4hBashIsSources                      | return 0 if there are sources, return 1 if there are no sources"
@@ -125,6 +131,5 @@ function h3lp () {
 
     # ugly as hell, rewrite! at least a little bit...
     grep -B 1 '^function ' ~/.bash.xx4h.d/*-cheatsheet-*.bash | perl -ne 'if (m(^#) .. m(^function )) {$line++; if ( $line > 1 ) {$function = $_; chomp $function} else {$help = $_}; if ($function && $help) {$function = $1 if $function =~ /^function\s+([^(]+).*/;$help = $1 if $help =~ /^# (.*)/;printf "  %-38s | %s\n", $function, $help} } else {$line=0; $function=""; $help=""}'
-
 
 }

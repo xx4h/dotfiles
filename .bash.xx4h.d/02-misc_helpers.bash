665a1667d1d6396ab9767bdbb29451ea68b483c2
Fabian 'xx4h' Melters <xx4h@xx4h.de>
2021-02-24 23:53:38 +0100

    Add some functions
    - video2gif
      create a gif from a video file
    - 3pointmen
      calculate body fat by jackson/pollock 3-point method

--- .bash.xx4h.d/02-misc_helpers.bash
+++ .bash.xx4h.d/02-misc_helpers.bash
@@ -39,3 +39,42 @@ function pjson() {
 
 # alembic python3
 alias alembic3='python -m alembic.config'
+
+# thanks to https://stackoverflow.com/a/10525920/1922402
+# Video to gif
+function video2gif() {
+    if [ -z "$1" -o -z "$2" ]; then
+        echo "Usage: video2gif /path/to/video /path/to/output.gif"
+        return
+    fi
+
+    if which ffmpeg 1>/dev/null; then
+        CMD="ffmpeg"
+    elif which avconv 1>/dev/null; then
+        CMD="avconv"
+    else
+        echo "We need either ffmpeg or avconv..."
+        return
+    fi
+    if ! which gifsicle 1>/dev/null; then
+        echo "We need gifsicle..."
+        return
+    fi
+
+    CONVDIR=`mktemp -d -t video2gif_XXXXXXXXXXXXX`
+    $CMD -i "$1" -t 10 "$CONVDIR/out%02d.gif"
+    gifsicle --delay=10 --loop "$CONVDIR"/*.gif > "$2"
+    rm -f "$CONVDIR/*.gif"
+    rm -r "$CONVDIR"
+}
+
+# 3-Point Fatcalc Men
+function 3pointmen() {
+    if [ -z "$1" ] || ( [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] ); then
+        echo "Usage: 3pointmen AGE BELLY BREAST LEG"
+    else
+        age="$1"
+        sum="$(( $2 + $3 + $4 ))"
+        python -c "print( ((4.95/( 1.10938 - (0.0008267 * $sum) + (0.0000016 * $sum**2) - (0.0002574 * $age))) - 4.5) * 100)"
+    fi
+}

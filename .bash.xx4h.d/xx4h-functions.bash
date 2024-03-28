a8c5e4217c517ff47ad3fa25efb81aa9f0da5b54
Fabian 'xx4h' Sylvester <xx4h@xx4h.de>
2024-03-28 22:40:24 +0100

    Add some h3lp messages for NvChad and tmux

--- .bash.xx4h.d/xx4h-functions.bash
+++ .bash.xx4h.d/xx4h-functions.bash
@@ -132,4 +132,11 @@ function h3lp () {
     # ugly as hell, rewrite! at least a little bit...
     grep -B 1 '^function ' ~/.bash.xx4h.d/*-cheatsheet-*.bash | perl -ne 'if (m(^#) .. m(^function )) {$line++; if ( $line > 1 ) {$function = $_; chomp $function} else {$help = $_}; if ($function && $help) {$function = $1 if $function =~ /^function\s+([^(]+).*/;$help = $1 if $help =~ /^# (.*)/;printf "  %-38s | %s\n", $function, $help} } else {$line=0; $function=""; $help=""}'
 
+    echo ""
+    echo "NvChad"
+    echo "  - On first run, start nvim and run ':Lazy sync'"
+
+    echo ""
+    echo "tmux"
+    echo "  - On first run, start tmux and run '<strg>+<space>-I'"
 }

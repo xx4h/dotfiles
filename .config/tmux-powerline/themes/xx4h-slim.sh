045144fe875cd569dc5d1f6fc82a2a1d2cb074db
Fabian 'xx4h' Sylvester <xx4h@xx4h.de>
2024-04-15 11:56:18 +0200

    Cleanup & minor improvements
    * Use MY_TMUX_POWERLIE_SEG_WEATHER_LAT/LON also for
      air segment
    * Introduce new MY_TMUX_POWERLINE_SEG_AIR_OPEN_WEATHER_API_KEY

--- .config/tmux-powerline/themes/xx4h-slim.sh
+++ .config/tmux-powerline/themes/xx4h-slim.sh
@@ -1,6 +1,8 @@
 # Default Theme
 # If changes made here does not take effect, then try to re-create the tmux session to force reload.
 
+TMUX_POWERLINE_SEG_AIR_COLOR="$(air_color)"
+
 TMUX_POWERLINE_SEPARATOR_LEFT_BOLD=""
 TMUX_POWERLINE_SEPARATOR_LEFT_THIN=""
 TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD=""
@@ -44,13 +46,13 @@ TMUX_POWERLINE_ACTIVITY_BACKGROUND_COLOR=${TMUX_POWERLINE_ACTIVITY_BACKGROUND_CO
 
 if [ -z "$TMUX_POWERLINE_WINDOW_STATUS_CURRENT" ]; then
 	TMUX_POWERLINE_WINDOW_STATUS_CURRENT=(
-		"#[fg=${TMUX_POWERLINE_THEME_ACTIVE_STATUS_BACKGROUND_COLOUR}]" \
-		"$TMUX_POWERLINE_THEME_LEFTSIDE_WINDOW_SEPARATOR" \
-		"#[bg=${TMUX_POWERLINE_THEME_ACTIVE_STATUS_BACKGROUND_COLOUR},fg=${TMUX_POWERLINE_THEME_ACTIVE_STATUS_FOREGROUND_COLOUR}]" \
-		" #I#F " \
-		"$TMUX_POWERLINE_THEME_MIDDLE_WINDOW_SEPARATOR" \
-		" #W " \
-		"#[fg=${TMUX_POWERLINE_THEME_ACTIVE_STATUS_BACKGROUND_COLOUR},bg=${TMUX_POWERLINE_THEME_BACKGROUND_COLOUR}]" \
+		"#[fg=${TMUX_POWERLINE_THEME_ACTIVE_STATUS_BACKGROUND_COLOUR}]"
+		"$TMUX_POWERLINE_THEME_LEFTSIDE_WINDOW_SEPARATOR"
+		"#[bg=${TMUX_POWERLINE_THEME_ACTIVE_STATUS_BACKGROUND_COLOUR},fg=${TMUX_POWERLINE_THEME_ACTIVE_STATUS_FOREGROUND_COLOUR}]"
+		" #I#F "
+		"$TMUX_POWERLINE_THEME_MIDDLE_WINDOW_SEPARATOR"
+		" #W "
+		"#[fg=${TMUX_POWERLINE_THEME_ACTIVE_STATUS_BACKGROUND_COLOUR},bg=${TMUX_POWERLINE_THEME_BACKGROUND_COLOUR}]"
 		"$TMUX_POWERLINE_THEME_RIGHTSIDE_WINDOW_SEPARATOR "
 	)
 fi
@@ -63,16 +65,16 @@ fi
 
 if [ -z "$TMUX_POWERLINE_WINDOW_STATUS_FORMAT" ]; then
 	TMUX_POWERLINE_WINDOW_STATUS_FORMAT=(
-		"#[fg=${TMUX_POWERLINE_THEME_INACTIVE_STATUS_BACKGROUND_COLOUR},bg=default,nobold,noitalics,nounderscore]" \
-    "#{?window_activity_flag,#[fg=${TMUX_POWERLINE_ACTIVITY_BACKGROUND_COLOR}],}#{?window_bell_flag,#[fg=${TMUX_POWERLINE_BELL_BACKGROUND_COLOR}],}" \
-		"$TMUX_POWERLINE_THEME_LEFTSIDE_WINDOW_SEPARATOR" \
-		"#[fg=${TMUX_POWERLINE_THEME_INACTIVE_STATUS_FOREGROUND_COLOUR},bg=${TMUX_POWERLINE_THEME_INACTIVE_STATUS_BACKGROUND_COLOUR},nobold,noitalics,nounderscore]" \
-    "#{?window_activity_flag,#[bg=${TMUX_POWERLINE_ACTIVITY_BACKGROUND_COLOR}],}#{?window_bell_flag,#[bg=${TMUX_POWERLINE_BELL_BACKGROUND_COLOR}],}" \
-		" #{?window_activity_flag,#[fg=${TMUX_POWERLINE_ACTIVITY_FOREGROUND_COLOR}],}#{?window_bell_flag,#[fg=${TMUX_POWERLINE_BELL_FOREGROUND_COLOR}],}#I#{?window_flags,#F, } " \
-		"#[fg=${TMUX_POWERLINE_THEME_INACTIVE_STATUS_BACKGROUND_COLOUR},bg=${TMUX_POWERLINE_THEME_BACKGROUND_COLOUR},nobold,noitalics,nounderscore]" \
-    "#{?window_activity_flag,#[fg=${TMUX_POWERLINE_ACTIVITY_BACKGROUND_COLOR}],}#{?window_bell_flag,#[fg=${TMUX_POWERLINE_BELL_BACKGROUND_COLOR}],}" \
-		"$TMUX_POWERLINE_THEME_RIGHTSIDE_WINDOW_SEPARATOR" \
-		"#[fg=${TMUX_POWERLINE_THEME_FOREGROUND_COLOUR},bg=${TMUX_POWERLINE_THEME_BACKGROUND_COLOUR}]" \
+		"#[fg=${TMUX_POWERLINE_THEME_INACTIVE_STATUS_BACKGROUND_COLOUR},bg=default,nobold,noitalics,nounderscore]"
+    "#{?window_activity_flag,#[fg=${TMUX_POWERLINE_ACTIVITY_BACKGROUND_COLOR}],}#{?window_bell_flag,#[fg=${TMUX_POWERLINE_BELL_BACKGROUND_COLOR}],}"
+		"$TMUX_POWERLINE_THEME_LEFTSIDE_WINDOW_SEPARATOR"
+		"#[fg=${TMUX_POWERLINE_THEME_INACTIVE_STATUS_FOREGROUND_COLOUR},bg=${TMUX_POWERLINE_THEME_INACTIVE_STATUS_BACKGROUND_COLOUR},nobold,noitalics,nounderscore]"
+    "#{?window_activity_flag,#[bg=${TMUX_POWERLINE_ACTIVITY_BACKGROUND_COLOR}],}#{?window_bell_flag,#[bg=${TMUX_POWERLINE_BELL_BACKGROUND_COLOR}],}"
+		" #{?window_activity_flag,#[fg=${TMUX_POWERLINE_ACTIVITY_FOREGROUND_COLOR}],}#{?window_bell_flag,#[fg=${TMUX_POWERLINE_BELL_FOREGROUND_COLOR}],}#I#{?window_flags,#F, } "
+		"#[fg=${TMUX_POWERLINE_THEME_INACTIVE_STATUS_BACKGROUND_COLOUR},bg=${TMUX_POWERLINE_THEME_BACKGROUND_COLOUR},nobold,noitalics,nounderscore]"
+    "#{?window_activity_flag,#[fg=${TMUX_POWERLINE_ACTIVITY_BACKGROUND_COLOR}],}#{?window_bell_flag,#[fg=${TMUX_POWERLINE_BELL_BACKGROUND_COLOR}],}"
+		"$TMUX_POWERLINE_THEME_RIGHTSIDE_WINDOW_SEPARATOR"
+		"#[fg=${TMUX_POWERLINE_THEME_FOREGROUND_COLOUR},bg=${TMUX_POWERLINE_THEME_BACKGROUND_COLOUR}]"
 		" #W "
 	)
 fi
@@ -108,46 +110,46 @@ fi
 
 if [ -z "$TMUX_POWERLINE_LEFT_STATUS_SEGMENTS" ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"status_icon 148 234 '' '' '' '' separator_disable" \
-		"tmux_session_info 148 234" \
-		"hostname 33 0" \
-		"vpn 39 0" \
-		"kubernetes_context 45 0" \
-		#"mode_indicator 165 0" \
-		#"ifstat 30 255" \
-		#"ifstat_sys 30 255" \
-		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		#"wan_ip 24 255" \
-		"vcs_branch 51 88" \
-		"vcs_rootpath 123 17" \
-		"vcs_compare 60 255" \
-		"vcs_staged 64 255" \
-		"vcs_modified 9 255" \
-		"vcs_others 245 0" \
-		#"vcs_revision 60 0" \
+		"status_icon 148 234 '' '' '' '' separator_disable"
+		"tmux_session_info 148 234"
+		"hostname 33 0"
+		"vpn 39 0"
+		"kubernetes_context 45 0"
+		#"mode_indicator 165 0"
+		#"ifstat 30 255"
+		#"ifstat_sys 30 255"
+		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}"
+		#"wan_ip 24 255"
+		"vcs_branch 51 88"
+		"vcs_rootpath 123 17"
+		"vcs_compare 60 255"
+		"vcs_staged 64 255"
+		"vcs_modified 9 255"
+		"vcs_others 245 0"
+		#"vcs_revision 60 0"
 	)
 fi
 
 if [ -z "$TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS" ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
-		#"earthquake 3 0" \
-		"pwd 123 0" \
-		#"macos_notification_count 29 255" \
-		#"mailcount 9 255" \
-		"now_playing 51 0" \
-		#"cpu 240 136" \
-		#"load 237 167" \
-		#"tmux_mem_cpu_load 234 136" \
-		#"battery 137 127" \
-		#"air ${TMUX_POWERLINE_SEG_AIR_COLOR} 255" \
-		"weather 45 0" \
-		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
-		#"xkb_layout 125 117" \
-		"date_week 39 0" \
-		"date_day 39 0 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"date 39 0 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"time 39 0 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"mode_indicator 33 0" \
+		#"earthquake 3 0"
+		"pwd 123 0"
+		#"macos_notification_count 29 255"
+		#"mailcount 9 255"
+		"now_playing 51 0"
+		#"cpu 240 136"
+		#"load 237 167"
+		#"tmux_mem_cpu_load 234 136"
+		#"battery 137 127"
+		#"air ${TMUX_POWERLINE_SEG_AIR_COLOR} 255"
+		"weather 45 0"
+		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}"
+		#"xkb_layout 125 117"
+		"date_week 39 0"
+		"date_day 39 0 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
+		"date 39 0 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
+		"time 39 0 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
+		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
+		"mode_indicator 33 0"
 	)
 fi

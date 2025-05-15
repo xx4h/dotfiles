d7c4f6e80a53f89ee9fd6d2f93c9f6d93af45037
Fabian 'xx4h' Sylvester <xx4h@xx4h.de>
2025-05-15 09:29:03 +0200

    Move tmux-powerline segments out of git control
    * update tool versions for flux and hctl

--- .bash.xx4h.d/00-color-aliases.bash
+++ .bash.xx4h.d/00-color-aliases.bash
@@ -1,5 +1,5 @@
 # Escape Opening Foreground/Background ; 5 (just a static, idk) ; 256 colors color + m at the end
-# \e     [       38/48                 ; 5                      ; 11m
+# \[\033     [       38/48                 ; 5                      ; 11m
 #
 # always remove all spaces from the above code
 # use xx4hBashGet256Colors to get the color codes for the above example
@@ -9,8 +9,8 @@ YELLOW="\[\033[38;5;220m\]"
 RED="\[\033[31m\]"
 GREEN="\[\033[32m\]"
 BOLD_GREEN="\[\033[1;32m\]"
-ORANGE="\[\e[0;33m\]"
-BOLD_ORANGE="\[\e[1;33m\]"
+ORANGE="\[\033[0;33m\]"
+BOLD_ORANGE="\[\033[1;33m\]"
 BLUE="\[\033[34m\]"
 LIGHT_GRAY="\[\033[37m\]"
 LIGHT_BLUE="\[\033[94m\]"
@@ -18,16 +18,16 @@ LIGHT_CYAN="\[\033[96m\]"
 WHITE="\[\033[37m\]"
 CYAN="\[\033[36m\]"
 
-bgreen="\[\e[1;32m\]"
-yellow="\[\e[0;93m\]"
-lblue="\[\e[0;94m\]"
+bgreen="\[\033[1;32m\]"
+yellow="\[\033[0;93m\]"
+lblue="\[\033[0;94m\]"
 
 
 COLOR_NONE="\[\033[0m\]"
 DEFAULT_COLOR="\[\033[39m\]"
 
-BOLD="\[\e[1m\]"
+BOLD="\[\033[1m\]"
 
 show_colour() {
-    perl -e 'foreach $a(@ARGV){print "\e[48;2;".join(";",unpack("C*",pack("H*",$a)))."m         \e[49m "};print "\n"' "$@"
+    perl -e 'foreach $a(@ARGV){print "\[\033[48;2;".join(";",unpack("C*",pack("H*",$a)))."m         \[\033[49m "};print "\n"' "$@"
 }
--- .config/tmux-powerline/config.sh
+++ .config/tmux-powerline/config.sh
@@ -3,53 +3,8 @@
 #	 vi: foldmarker={,} foldmethod=marker foldlevel=0 tabstop=4 filetype=sh
 # }
 
-export TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"status_icon 148 234 default_separator no_sep_bg_color no_sep_fg_color no_spacing_disable separator_disable"
-		"tmux_session_info 148 234"
-		"hostname 33 0"
-		"vpn 39 0"
-		"kubernetes_context 45 0"
-		#"mode_indicator 165 0"
-		#"ifstat 30 255"
-		#"ifstat_sys 30 255"
-		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}"
-		#"wan_ip 24 255"
-		"vcs_branch 51 88"
-		"vcs_rootpath 123 17"
-		"vcs_compare 60 255"
-		"vcs_staged 64 255"
-		"vcs_modified 9 255"
-		"vcs_others 245 0"
-		#"vcs_revision 60 0"
-)
-
-export TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
-		#"earthquake 3 0"
-		"pwd 123 0"
-		#"macos_notification_count 29 255"
-		#"mailcount 9 255"
-		"now_playing 51 0"
-		#"cpu 240 136"
-		#"load 237 167"
-		#"tmux_mem_cpu_load 234 136"
-		#"battery 137 127"
-		#"air ${TMUX_POWERLINE_SEG_AIR_COLOR} 255"
-		"weather 45 0"
-		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}"
-		#"xkb_layout 125 117"
-		"date_week 39 0 default_separator no_sep_bg_color no_sep_fg_color right_disable"
-		"date_day 39 0 󰿟 no_sep_bg_color 45 both_disable"
-		"date 39 0 󰿟 no_sep_bg_color 45 both_disable"
-		"time 39 0 󰿟 no_sep_bg_color 45 left_disable"
-		#"date_week 39 0"
-		#"date_day 39 0 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN} no_sep_bg_color 33"
-		#"date 39 0 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN} no_sep_bg_color 33"
-		#"time 39 0 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN} no_sep_bg_color 33"
-		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
-		"mode_indicator 33 0"
-		"github_notifications 148 234"
-)
-
+export TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=$MY_TMUX_POWERLINE_LEFT_STATUS_SEGMENTS
+export TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=$MY_TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS
 
 # General {
 	# Show which segment fails and its exit code.
